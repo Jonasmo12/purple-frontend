6057aab300e304d3f22b2bc1ac61920ff9035542
Jonas Moloto <jonasmoloto@outlook.com>
2023-05-15 12:57:24 +0200

    api call to backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
 import './App.css';
 import Nav from './Nav.js';
 import Footer from './Footer.js';
+import { useState, useEffect } from 'react';
+
+const Patient = () => {
+    const [ patient, setPatient] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:8080/patient/1")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setPatient(data)
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+
+    })
+
+    return (
+        <div>
+            <h1>Basic information</h1>
+            <b>Patient ID: </b> {patient.patientID} <br></br>
+            <b>Username: </b> {patient.username} <br></br>
+            <b>Phone: </b> {patient.phone} <br></br>
+            <b>Age:</b> {patient.age} <br></br>
+            <b>Date of Birth: </b> {patient.dob} <br></br>
+            <b>Gender: </b> {patient.gender} <br></br>
+
+            <h1>Emergency Contacts</h1>
+            <h1>Allergies</h1>
+            <h1>Medications</h1>
+            <h1>Diagnoses</h1>
+
+        </div>
+        
+    );
+}
 
 function App() {
     return (
@@ -11,7 +47,7 @@ function App() {
 
             <div class="container patient-home">
                 <div class="row">
-                    <h1>Patient Home</h1>
+                    <Patient />
                 </div>
             </div>
 
